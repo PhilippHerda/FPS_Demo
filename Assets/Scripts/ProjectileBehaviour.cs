a829e6d8e681b309e405a20f6c02bd84e6ad363a
Philipp Herda <philippherda@hotmail.com>
2023-11-12 16:37:10 +0100

    Finalize projectiles

--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -7,5 +7,19 @@ public class ProjectileBehaviour : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
+
+        //if (collision.transform.CompareTag("Player"))
+        //{
+        //    GameObject player = other.gameObject;
+
+        //    player.GetComponent<Rigidbody>().AddForce(Vector3.up * 1000f, ForceMode.Acceleration);
+        //}
+
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            GameObject enemy = collision.gameObject;
+
+            enemy.GetComponent<BotAI>().TakeDamage(1);
+        }
     }
 }
